532339085489004f46accc67969151613702e073
Dong Hee Lee <dhblee0618@gmail.com>
2025-01-27 09:37:49 +1100

    added

--- space-tourism-website-main/js/script.js
+++ space-tourism-website-main/js/script.js
@@ -45,7 +45,7 @@ let crewData = [];
 let technologyData = [];
 
 
-fetch('data.json')
+fetch('starter-code/data.json')
   .then(response => response.json())
   .then(data => {
     window.destinationsData = data.destinations; 
